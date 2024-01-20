1a1618ba1f09f57539f2588f5f423564ed466f46
Denis Prohorchuk <denprohorchuk@gmail.com>
2024-01-20 18:26:11 +0300

    feat: add solution getIntervalArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,17 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  if (typeof start !== 'number' || typeof end !== 'number' || start > end) {
+    throw new Error('start and end must be numbers');
+  }
+  const step = 1;
+  return Array.from(
+    {
+      length: (end - start) / step + 1,
+    },
+    (_, i) => start + i * step
+  );
 }
 
 /**
