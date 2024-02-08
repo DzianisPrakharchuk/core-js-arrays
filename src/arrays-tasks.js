67ee016282bb5d7a9029d42ae31ad9926b9069cf
Denis Prohorchuk <denprohorchuk@gmail.com>
2024-02-08 11:44:50 +0300

    feat: add solution isSameLenght and isValueEqualsIndex tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -162,8 +162,11 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const firstElementLength = arr[0].length;
+  return arr.every((element) => {
+    return element.length === firstElementLength;
+  });
 }
 
 /**
@@ -177,8 +180,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((element, index) => element === index);
 }
 
 /**
